92f5e2c2cc72eafe52549b92e7c0f642cb26b24e
hafizhdary <hafizh.dary@defghi.id>
2022-11-21 11:35:55 +0700

    remove login

--- src/Table/Table.js
+++ src/Table/Table.js
@@ -365,7 +365,13 @@ function Table({ DATA, COLUMNS, pageNum, setPageNum, isPrevious, isNext, schoolR
                 <button className="btn_tools" onClick={downloadAsCSV2}><FaTable/>Download as CSV</button>
                 {!firstCur ? <p>Tunggu sebentar...</p> : null}
                 <CSVLink data={dataList} filename={'Rekap_Data_Sekolah.csv'} ref={csvLink} target="_blank" className="hidden"/>
-            </div> : null }
+            </div> : 
+            <div>
+                <button className="btn_tools" onClick={downloadAsCSV2}><FaTable/>Download as CSV</button>
+                {!firstCur ? <p>Tunggu sebentar...</p> : null}
+                <CSVLink data={dataList} filename={'Rekap_Data_Sekolah.csv'} ref={csvLink} target="_blank" className="hidden"/>
+            </div>
+            }
         </div>
         {/* <div class="table_props_2">
             <p>Jumlah Sekolah: <span>0 |</span></p>
